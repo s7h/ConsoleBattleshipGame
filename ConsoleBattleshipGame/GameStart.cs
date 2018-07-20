aa995db195a1d9c601ee5bc2ea1d35e6a1c08527
Siddharth Singh <siddharth.s1312@gmail.com>
2018-07-20 17:00:05 +0530

    Initializing the repo

---
+++ ConsoleBattleshipGame/GameStart.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleBattlefield
+{
+    public class GameStart
+    {
+        public static void Main(string[] args)
+        {
+            var battlefield = UnityConfig.RegisterDependencies();
+
+            //battlefield.RunBattlefield();
+
+            Console.WriteLine("Game Over! Press Any Key to Exit.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleBattleshipGame/UnityConfig.cs
@@ -0,0 +1,17 @@
+﻿using Unity;
+
+namespace ConsoleBattlefield
+{
+    public class UnityConfig
+    {
+        public static BattleshipExecutor RegisterDependencies()
+        {
+            var container = new UnityContainer();
+            container.RegisterType<BattleshipExecutor>();
+
+            //Register all dependencies
+
+            return container.Resolve<BattleshipExecutor>();
+        }
+    }
+}
